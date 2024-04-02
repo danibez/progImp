a55366200b8a595e4311cd74202faa9876322b98
aluno <aluno0@unicap.br>
2024-04-01 21:35:18 -0300

    matriz

---
+++ Matrix.java
@@ -0,0 +1,16 @@
+public class Matrix {
+    public static void main(String[] args) {
+        int[][] m = new int[3][5];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                m[i][j] = i+j;
+            }
+        }
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.println(m[i][j]);
+            }
+        }
+    }
+}
---
+++ MatrixStatic.java
@@ -0,0 +1,19 @@
+public class MatrixStatic {
+    public static void main(String[] args) {
+        int[][] mat = {
+                     {1,2,3},
+                     {4,5,6},
+                     {7,8,9}
+                    };
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+      
+
