2de0109c73f062692d20262bcc365b7f1f2fac7c
aluno <aluno0@unicap.br>
2024-04-11 21:46:08 -0300

    matrix diagonais

---
+++ matriz/MatrixDiagonal.java
@@ -0,0 +1,20 @@
+package matriz;
+
+public class MatrixDiagonal {
+    public static void main(String[] args) {
+        int[][] mat = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (i+j == mat.length - 1)
+                    System.out.println(mat[i][j]);
+                
+            }
+        }
+
+    }    
+}
